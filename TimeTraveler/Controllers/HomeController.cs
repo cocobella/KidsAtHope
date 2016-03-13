f98cd1cf2a47cc63ab9d8fe5dc87425eedb034e4
NotTheNull <objnotnull@live.com>
2016-03-12 19:44:25 -0700

    Added postback handler for the Index page

--- TimeTraveler/Controllers/HomeController.cs
+++ TimeTraveler/Controllers/HomeController.cs
@@ -12,8 +12,27 @@ namespace TimeTraveler.Controllers
 
         public ActionResult Index()
 		{
+            if (this.TheTraveler == null) this.TheTraveler = new TimeTravelerDetail();
+            SessionBag.Current.TheTraveler = this.TheTraveler;
+
 			return View();
 		}
+
+        [HttpPost]
+        public ActionResult Index(string name, string grade)
+        {
+            this.TheTraveler = SessionBag.Current.TheTraveler as TimeTravelerDetail;
+            if (this.TheTraveler == null) this.TheTraveler = new TimeTravelerDetail();
+            this.TheTraveler.Name = name;
+
+            int g = 0;
+            int.TryParse(grade, out g);
+            this.TheTraveler.Grade = g;
+
+            SessionBag.Current.TheTraveler = this.TheTraveler;
+
+            return RedirectToAction("HomeAndFamily");
+        }
 
 		public ActionResult About()
 		{
