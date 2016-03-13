2da355d79a62a0d50a47f08aad05ce8db2446102
NotTheNull <objnotnull@live.com>
2016-03-13 13:47:23 -0700

    Removing unnecessary code

--- TimeTraveler/Content/timetraveler.js
+++ TimeTraveler/Content/timetraveler.js
@@ -55,39 +55,6 @@ $(document).ready(function () {
     });
         
 
-    $('input[name="whereLive"]').click(function () {
-
-        var span = document.getElementById('modalHomeChoice');
-        var theOption = $('input[name="whereLive"]:checked').val();
-
-        switch (theOption) {
-            case "downtown":
-                span.innerHTML = "downtown";
-                $("#modalHomeVideo").attr("src", "http://www.thetimetravelerapp.com/video/dest_1_1/downtown.mp4");
-                break;
-            case "mountain":
-                span.innerHTML = "in the mountains";
-                $("#modalHomeVideo").attr("src", "http://www.thetimetravelerapp.com/video/dest_1_1/mountain.mp4");
-                break;
-            case "ranch":
-                span.innerHTML = "on a ranch";
-                $("#modalHomeVideo").attr("src", "http://www.thetimetravelerapp.com/video/dest_1_1/ranch.mp4");
-                break;
-            case "ocean":
-                span.innerHTML = "by the ocean";
-                $("#modalHomeVideo").attr("src", "http://www.thetimetravelerapp.com/video/dest_1_1/ocean.mp4");
-                break;
-            case "snow":
-                span.innerHTML = "where it snows";
-                $("#modalHomeVideo").attr("src", "http://www.thetimetravelerapp.com/video/dest_1_1/snow.mp4");
-                break;
-            case "suburb":
-                span.innerHTML = "in the suburb";
-                $("#modalHomeVideo").attr("src", "http://www.thetimetravelerapp.com/video/dest_1_1/suburb.mp4");
-                break;
-        }
-
-    });
 
 });
 
